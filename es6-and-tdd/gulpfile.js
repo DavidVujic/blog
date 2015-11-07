ab4c1afd0eef5a3b9a70d310a8115ae51db9c5d6
David Vujic <david@vujic.se>
2015-11-08 00:50:18 +0100

    editor formatting changes

--- es6-and-tdd/gulpfile.js
+++ es6-and-tdd/gulpfile.js
@@ -11,82 +11,82 @@ var codeFolder = 'lib';
 var dest = '/script/transpiled';
 var files = '/script/es6/**/*.js';
 var babelOptions = {
-  presets: ['es2015'],
-  plugins: ["transform-es2015-modules-amd"]
+	presets: ['es2015'],
+	plugins: ["transform-es2015-modules-amd"]
 };
 
 var tests = {
-  source: testFolder + files,
-  destination: testFolder + dest
+	source: testFolder + files,
+	destination: testFolder + dest
 };
 
 var code = {
-  source: codeFolder + files,
-  destination: codeFolder + dest
+	source: codeFolder + files,
+	destination: codeFolder + dest
 };
 
 var transpile = function(paths) {
-  return gulp.src(paths.source)
-    .pipe(newer(paths.destination))
-    .pipe(babel(babelOptions))
-    .pipe(gulp.dest(paths.destination));
+	return gulp.src(paths.source)
+		.pipe(newer(paths.destination))
+		.pipe(babel(babelOptions))
+		.pipe(gulp.dest(paths.destination));
 };
 
 var lint = function(paths) {
-  return gulp.src(paths.source)
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(jshint.reporter('fail'));
+	return gulp.src(paths.source)
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
+		.pipe(jshint.reporter('fail'));
 };
 
 gulp.task('copy vendor scripts', function() {
-  gulp.src([
-      'node_modules/jquery/dist/jquery.min.js',
-      'node_modules/requirejs/require.js',
-      'node_modules/babel-polyfill/dist/polyfill.min.js'
-    ])
-    .pipe(gulp.dest('lib/script/vendor'));
+	gulp.src([
+			'node_modules/jquery/dist/jquery.min.js',
+			'node_modules/requirejs/require.js',
+			'node_modules/babel-polyfill/dist/polyfill.min.js'
+		])
+		.pipe(gulp.dest('lib/script/vendor'));
 
-  gulp.src([
-      'node_modules/qunitjs/qunit/qunit.css',
-      'node_modules/qunitjs/qunit/qunit.js'
-    ])
-    .pipe(gulp.dest('tests/script/qunit'));
+	gulp.src([
+			'node_modules/qunitjs/qunit/qunit.css',
+			'node_modules/qunitjs/qunit/qunit.js'
+		])
+		.pipe(gulp.dest('tests/script/qunit'));
 });
 
 gulp.task('transpile tests', function() {
-  return transpile(tests);
+	return transpile(tests);
 });
 
 gulp.task('transpile source', function() {
-  return transpile(code);
+	return transpile(code);
 });
 
 gulp.task('lint-tests', function() {
-  return lint(tests);
+	return lint(tests);
 });
 
 gulp.task('lint-source', function() {
-  return lint(code);
+	return lint(code);
 });
 
 gulp.task('qunit', function() {
-  qunit(testFolder + '/testrunner.html', {
-    'verbose': false
-  });
+	qunit(testFolder + '/testrunner.html', {
+		'verbose': false
+	});
 });
 
 gulp.task('watch', function() {
-  gulp.watch(tests.source, ['transpile tests', 'lint-tests', 'qunit']);
-  gulp.watch(code.source, ['transpile source', 'lint-source', 'qunit']);
+	gulp.watch(tests.source, ['transpile tests', 'lint-tests', 'qunit']);
+	gulp.watch(code.source, ['transpile source', 'lint-source', 'qunit']);
 });
 
 gulp.task('default', [
-  'copy vendor scripts',
-  'transpile tests',
-  'transpile source',
-  'lint-tests',
-  'lint-source',
-  'qunit',
-  'watch'
+	'copy vendor scripts',
+	'transpile tests',
+	'transpile source',
+	'lint-tests',
+	'lint-source',
+	'qunit',
+	'watch'
 ]);
